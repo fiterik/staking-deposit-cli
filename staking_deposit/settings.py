a0f797a67ba8b73d7edcf1703f71e252b1d699d2
Erik Tóth <fiterik@gmail.com>
2025-06-23 12:58:50 +0200

    feat: add localnet, devnet, testnet and mainnet

--- staking_deposit/settings.py
+++ staking_deposit/settings.py
@@ -1,7 +1,7 @@
 from typing import Dict, NamedTuple
 from eth_utils import decode_hex
 
-DEPOSIT_CLI_VERSION = '3.0.2'
+DEPOSIT_CLI_VERSION = '3.1'
 
 
 class BaseChainSetting(NamedTuple):
@@ -9,18 +9,41 @@ class BaseChainSetting(NamedTuple):
     GENESIS_FORK_VERSION: bytes
     GENESIS_VALIDATORS_ROOT: bytes
 
-ITXTESTNET = 'itxtestnet'
+
+ITXLOCALNET = 'itxlocalnet' # 532100
+ITXDEVNET = 'itxdevnet' # 53210
+ITXTESTNET = 'itxtestnet' # 5321
+ITXMAINNET = 'itxmainnet' # 1235
+
+# 4954584C -> ITXL | Chain ID 532100
+# 49545844 -> ITXD | Chain ID 53210
+# 49545854 -> ITXT | Chain ID 5321
+# 49545800 -> ITXM | Chain ID 1235
+
+ItxLocalSetting = BaseChainSetting(
+    NETWORK_NAME=ITXLOCALNET, GENESIS_FORK_VERSION=bytes.fromhex('4954584C'),
+    GENESIS_VALIDATORS_ROOT=bytes.fromhex('0000000000000000000000000000000000000000000000000000000000000000'))
+
+ItxDevnetSetting = BaseChainSetting(
+    NETWORK_NAME=ITXDEVNET, GENESIS_FORK_VERSION=bytes.fromhex('49545844'),
+    GENESIS_VALIDATORS_ROOT=bytes.fromhex('0000000000000000000000000000000000000000000000000000000000000000'))
 
 ItxTestnetSetting = BaseChainSetting(
-    NETWORK_NAME=ITXTESTNET, GENESIS_FORK_VERSION=bytes.fromhex('49545800'),
+    NETWORK_NAME=ITXTESTNET, GENESIS_FORK_VERSION=bytes.fromhex('49545854'),
+    GENESIS_VALIDATORS_ROOT=bytes.fromhex('0000000000000000000000000000000000000000000000000000000000000000'))
+
+ItxMainnetSetting = BaseChainSetting(
+    NETWORK_NAME=ITXMAINNET, GENESIS_FORK_VERSION=bytes.fromhex('49545800'),
     GENESIS_VALIDATORS_ROOT=bytes.fromhex('0000000000000000000000000000000000000000000000000000000000000000'))
 
 ALL_CHAINS: Dict[str, BaseChainSetting] = {
+    ITXLOCALNET: ItxLocalSetting,
+    ITXDEVNET: ItxDevnetSetting,
     ITXTESTNET: ItxTestnetSetting,
+    ITXMAINNET: ItxMainnetSetting,
 }
 
-
-def get_chain_setting(chain_name: str = ITXTESTNET) -> BaseChainSetting:
+def get_chain_setting(chain_name: str = ITXLOCALNET) -> BaseChainSetting:
     return ALL_CHAINS[chain_name]
 
 
